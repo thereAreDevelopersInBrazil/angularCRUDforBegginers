699a92b5d9fddbf615881a612c98754f8fedd0a3
Natanael Felipe Justino de Sousa <natanaelfelipejustinodesousa@hotmail.com>
2016-01-19 20:27:16 -0200

    Basic example of angular-material form components | Exemplo basico de uso dos componentes de formulario do angular-material

--- app/js/aula1-config.js
+++ app/js/aula1-config.js
@@ -1,3 +1,8 @@
-//angular.module('aula1', []);//declaração do module principal
-//angular.module('aula2', []);//declaração do module principal
-angular.module('aula3', ['ngMessages', 'toastr', 'ui.grid']);//declaração do module principal
+angular.module('aula1', []);//declaração do module principal
+angular.module('aula2', []);//declaração do module principal
+angular.module('aula3', [
+    'ngMessages',
+    'toastr',
+    'ui.grid',
+    'ngMaterial'
+]);
